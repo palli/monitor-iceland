d6c8b02dc2ab32cb90d7a7ff5babb857edba582b
Pall Sigurdsson <palli@opensource.is>
2014-02-12 13:59:20 +0000

    Update check_domain_dns.py

--- nagios-plugins/check_domain_dns.py
+++ nagios-plugins/check_domain_dns.py
@@ -14,8 +14,8 @@ def check_dns(host_name, expected_address=None, expect_authority=False):
     """
     if not expected_address:
         expected_address = socket.gethostbyname(host_name)
-    check_command = "/usr/lib64/nagios/plugins/check_dns -H '{host_name}' -A '{expected_address}'"
-    code, stdout, stderr = pynag.Utils.runCommand(check_command.format(host_name=host_name, expected_address=expected_address))
+    check_command = "/usr/lib64/nagios/plugins/check_dns -H '{host_name}' -A '{expected_address}'".format(**locals())
+    code, stdout, stderr = pynag.Utils.runCommand(check_command)
     output = pynag.Utils.PluginOutput(stdout)
     output.exit_code = code
 
@@ -26,35 +26,41 @@ def check_dns(host_name, expected_address=None, expect_authority=False):
     return output
 
 
-def check_many_dns_servers(domain):
+def check_many_dns_servers():
     """ Check all nameservers of a single domain. Returns a PluginHelper instance
     """
     pynag.Utils.PluginOutput.parsed_perfdata = pynag.Utils.PerfData()
-    helper = pynag.Plugins.PluginHelper()
-    helper.add_option("--domain", dest="domain", default="okbeint.is")
-    helper.parse_arguments()
-    helper.show_legacy = True
     nameservers = dns.resolver.query(helper.options.domain, 'NS')
+    nameservers = nameservers or []
     nameservers = map(lambda x: str(x), nameservers)
     response_times = []
-    helper.add_summary("Checked %s nameservers" % (len(nameservers)))
-    if not nameservers:
-        return helper
     for i in nameservers:
         pluginoutput = check_dns(i)
-        helper.status(pluginoutput.exit_code)
-        helper.add_long_output(pluginoutput.summary)
+        exit_code = pluginoutput.exit_code
+        friendly_code = pynag.Plugins.state_text.get(exit_code, 'Unknown')
+        helper.status(pynag.Plugins.state.get(exit_code, 2))
+        helper.add_long_output("")
+        helper.add_long_output("Checking %s: " % i)
+        helper.add_long_output("* Exit code: %s (status=%s)" % (exit_code, friendly_code))
+        helper.add_long_output("* Output: %s" % pluginoutput.summary)
         time = pluginoutput.parsed_perfdata.get_perfdatametric('time')
+        if not time:
+            continue
         time.label = "%stime" % (i)
         response_times.append(float(time.value))
         helper._perfdata.metrics.append(time)
+    helper.add_summary("%s out of %s nameservers are responding" % (len(response_times), len(nameservers)))
+    if not response_times:
+        helper.status(2)
+        return
     average_response_time = sum(response_times) / len(response_times)
     helper.add_metric('average response time', average_response_time, uom='s')
     helper.add_metric('highest response time', max(response_times), uom='s', warn="1..inf", crit="2..inf")
-    helper.check_all_metrics()
-    return helper
 
 
-helper = check_many_dns_servers('ok.is')
+helper = pynag.Plugins.PluginHelper()
+helper.add_option("--domain", dest="domain", default="example.com")
+helper.parse_arguments()
+check_many_dns_servers()
 helper.check_all_metrics()
 helper.exit()
